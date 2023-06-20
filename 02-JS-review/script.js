8c721b69a8b14db5ef059b267027db110fed2f2e
Gustavo de Paula <gdepaula@gmail.com>
2023-06-20 11:39:39 -0300

    Rest/Spread Operator

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -144,7 +144,7 @@ function getBook(id) {
 }
 
 // Destrcucturing
-const book = getBook(2);
+const book = getBook(1);
 book;
 // const title = book.title;
 // const author = book.author;
@@ -157,5 +157,18 @@ console.log(author, title, genres);
 // const primaryGenre = genres[0];
 // const secondaryGenre = genres[1];
 
-const [primaryGenre, secondaryGenre] = genres;
-console.log(primaryGenre, secondaryGenre);
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, secondaryGenre, otherGenres);
+
+const newGenres = [...genres, "epic fantasy"];
+newGenres;
+
+const updatedBook = {
+  ...book,
+  // Adding a new property
+  moviePublicationDate: "2001-12-19",
+
+  // Overwriting an exisitng property
+  pages: 1210,
+};
+updatedBook;
