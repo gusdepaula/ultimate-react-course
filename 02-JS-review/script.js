fb8d3f676938be7bc42bb35feff703e1b6bd7d2d
Gustavo de Paula <gdepaula@gmail.com>
2023-06-21 11:33:03 -0300

    Asynchronous JavaScript: Async/Await

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -289,8 +289,18 @@ const booksAfterUpdate = booksAfterDelete.map((book) =>
 booksAfterUpdate;
 
 */
-console.log(fetch("https://jsonplaceholder.typicode.com/todos"));
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
