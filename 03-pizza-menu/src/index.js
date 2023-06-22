bbfba67eae284c602823b00bcbfbbaefbe94ac0a
Gustavo de Paula <gdepaula@gmail.com>
2023-06-22 16:01:19 -0300

    JavaScript Logic in Components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -83,6 +83,16 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour > openHour && hour <= closeHour;
+  console.log(isOpen);
+  // if (hour > openHour && hour <= closeHour) {
+  //   alert("We're currently open");
+  // } else {
+  //   alert("Sorry we're closed");
+  // }
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
   );
