fbe672e4bfa97311f6e80a1738e3600172e9658c
Gustavo de Paula <gdepaula@gmail.com>
2023-06-26 11:31:12 -0300

    React Fragments

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -76,13 +76,19 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
-        <p>We're still working on our menu. Please come back later</p>
+        <p>We're still working on our menu. Please come back later 😀</p>
       )}
 
       {/*<Pizza
