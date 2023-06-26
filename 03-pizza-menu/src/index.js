cade9d773184f14fdc09ca362b3dd4942db9044f
Gustavo de Paula <gdepaula@gmail.com>
2023-06-26 10:46:42 -0300

    Rendering Lists

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -72,7 +72,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/*<Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -83,7 +88,7 @@ function Menu() {
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/funghi.jpg"
         price={12}
-      />
+      />*/}
     </main>
   );
 }
@@ -91,14 +96,14 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
