ba12bc427daa1976dfaf132a55966b27eb1e8d07
Gustavo de Paula <gdepaula@gmail.com>
2023-06-27 12:26:19 -0300

    Updating State Based on Current State

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -12,17 +12,20 @@ export default function App() {
 
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      setStep((s) => s - 1);
     }
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
       {isOpen && (
