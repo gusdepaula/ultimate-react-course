e56da8eaf198d1516b3cd9d000ca275399d1c487
Gustavo de Paula <gdepaula@gmail.com>
2023-06-27 10:45:18 -0300

    Handling Events the React Way

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -18,10 +26,16 @@ export default function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950F2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950F2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950F2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
