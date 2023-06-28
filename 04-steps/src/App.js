e5eaf77c47c2d317a0838992dc8c8eb1b038b893
Gustavo de Paula <gdepaula@gmail.com>
2023-06-28 10:28:28 -0300

    A Vanilla JavaScript Implementation

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -24,7 +33,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -54,6 +63,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
