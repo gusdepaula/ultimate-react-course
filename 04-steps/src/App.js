493aacb8f8d1536c512b82b7bf47f340979a8ebc
Gustavo de Paula <gdepaula@gmail.com>
2023-07-03 09:58:29 -0300

    More Reasubility With the "children" Prop

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -44,9 +44,18 @@ function Steps() {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
           <div className="buttons">
             <Button bgColor="#7950F2" textColor="#fff" onClick={handlePrevious}>
               <span>👈🏻</span>Previous
@@ -57,6 +66,14 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}:</h3> {children}
     </div>
   );
 }
