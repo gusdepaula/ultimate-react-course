48a900af05d4afaeedd3f05bd0a63067c3a24042
Gustavo de Paula <gdepaula@gmail.com>
2023-07-07 10:11:20 -0300

    Using the Key Prop to Fiz Our Eat-'N-Split App

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -78,6 +78,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
