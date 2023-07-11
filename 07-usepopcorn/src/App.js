f06eabb088108d07a870727de7e73bb4349edc47
Gustavo de Paula <gdepaula@gmail.com>
2023-07-11 14:22:09 -0300

    Adding a New Effect: Changing Page Title

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -320,6 +320,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     },
     [selectedId]
   );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
