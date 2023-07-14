07d00d717e93c06adca952900033996bda7d9c6f
Gustavo de Paula <gdepaula@gmail.com>
2023-07-14 11:19:03 -0300

    Removing Boilerplate Code with Class Fields

--- 09-classy-weather/src/App-v2.js
+++ 09-classy-weather/src/App-v2.js
@@ -33,14 +33,19 @@ function formatDay(dateStr) {
 }
 
 class App extends React.Component {
-  state = {
-    location: "lisbon",
-    isLoading: false,
-    displayLocation: "",
-    weather: {},
-  };
+  constructor(props) {
+    super(props);
 
-  fetchWeather = async () => {
+    this.state = {
+      location: "lisbon",
+      isLoading: false,
+      displayLocation: "",
+      weather: {},
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  async fetchWeather() {
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -69,7 +74,7 @@ class App extends React.Component {
     } finally {
       this.setState({ isLoading: false });
     }
-  };
+  }
 
   render() {
     return (
@@ -89,7 +94,7 @@ class App extends React.Component {
         {this.state.weather.weathercode && (
           <Weather
             weather={this.state.weather}
-            location={this.state.displayLocation}
+            location={this.state.location}
           />
         )}
       </div>
