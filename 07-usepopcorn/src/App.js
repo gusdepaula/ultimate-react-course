0c241e93f58422714c661213a29373893fe3935a
Gustavo de Paula <gdepaula@gmail.com>
2023-07-14 12:13:42 -0300

    HTTPS, but requested an insecure resource

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -247,7 +247,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       setIsLoading(true);
       async function getMovieDetails() {
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
--- 07-usepopcorn/src/useMovies.js
+++ 07-usepopcorn/src/useMovies.js
@@ -17,7 +17,7 @@ export function useMovies(query) {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok)
