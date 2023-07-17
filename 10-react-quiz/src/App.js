2122fc9e861b7bfc17e46817378ac9cbd07c2db6
Gustavo de Paula <gdepaula@gmail.com>
2023-07-17 13:53:56 -0300

    The "React Quiz" app

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,15 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
