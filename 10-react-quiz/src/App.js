0a077e64ecb91992f852910cc1aa5bf9c03d27c0
Gustavo de Paula <gdepaula@gmail.com>
2023-07-17 15:19:33 -0300

    Starting a New Quiz

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -17,6 +18,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Unknonw action");
   }
@@ -40,7 +43,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
--- 10-react-quiz/src/StartScreen.js
+++ 10-react-quiz/src/StartScreen.js
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestions} question to test your React mastery</h3>
-      <button className="btn btn-ui">Let's start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
     </div>
   );
 }
