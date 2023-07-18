1cc41b9d5ee1a5b5bf5f88c503fc98666d1656f3
Gustavo de Paula <gdepaula@gmail.com>
2023-07-18 11:57:26 -0300

    Restarting a Quiz

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -46,6 +46,20 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      // return {
+      //   ...initialState,
+      //   question: state.questions,
+      //   status: "ready",
+      // };
+      return {
+        ...state,
+        points: 0,
+        highscore: 0,
+        index: 0,
+        answer: null,
+        status: "ready",
+      };
     default:
       throw new Error("Unknonw action");
   }
@@ -104,6 +118,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
--- 10-react-quiz/src/components/FinishedScreen.js
+++ 10-react-quiz/src/components/FinishedScreen.js
@@ -1,4 +1,4 @@
-function FinishedScreen({ points, maxPossiblePoints, highscore }) {
+function FinishedScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "🥇";
@@ -11,10 +11,15 @@ function FinishedScreen({ points, maxPossiblePoints, highscore }) {
     <>
       <p className="result">
         <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
-        {maxPossiblePoints})(
-        {Math.ceil(percentage)}%)
+        {maxPossiblePoints}({Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart quiz
+      </button>
     </>
   );
 }
