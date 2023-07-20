6b5cb343b2d7d746a43dda03e1c3002bd9f5d8b3
Gustavo de Paula <gdepaula@gmail.com>
2023-07-20 15:23:10 -0300

    Building the AppLayout

---
+++ 11-worldwise/src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
 import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>AppLayout</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
