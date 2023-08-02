61d67b9351db72072675f8963b97cc39823177bd
Gustavo de Paula <gdepaula@gmail.com>
2023-08-02 12:12:51 -0300

    Displaying a Loading Indicator

--- 16-fast-react-pizza/src/features/menu/Menu.jsx
+++ 16-fast-react-pizza/src/features/menu/Menu.jsx
@@ -4,7 +4,6 @@ import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
   return (
     <ul>
       {menu.map((pizza) => (
--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
