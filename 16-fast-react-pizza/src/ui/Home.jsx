634991bc7572e29f3aa1fe6b9b33d921e8b1f49e
Gustavo de Paula <gdepaula@gmail.com>
2023-08-03 12:02:17 -0300

    Working with Color

--- 16-fast-react-pizza/src/ui/Home.jsx
+++ 16-fast-react-pizza/src/ui/Home.jsx
@@ -1,10 +1,12 @@
 function Home() {
   return (
-    <div className="text-center text-xl font-semibold text-yellow-500">
+    <div className="text-center text-xl font-semibold text-stone-700">
       <h1>
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
     </div>
   );
