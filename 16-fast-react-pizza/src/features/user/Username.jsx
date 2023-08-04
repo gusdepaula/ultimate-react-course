e4fa535d6f83437dff7c9c03c67b1626d1d29ebc
Gustavo de Paula <gdepaula@gmail.com>
2023-08-04 09:30:59 -0300

    Using Flexbox

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Gustavo</div>;
+  return <div className="hidden text-sm font-semibold md:block">Gustavo</div>;
 }
 
 export default Username;
