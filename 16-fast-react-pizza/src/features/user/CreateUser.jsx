5754f080ca4e52b3ab58e8d5dadd3fbafc090b94
Gustavo de Paula <gdepaula@gmail.com>
2023-08-04 09:23:34 -0300

    Responsive Design

--- 16-fast-react-pizza/src/features/user/CreateUser.jsx
+++ 16-fast-react-pizza/src/features/user/CreateUser.jsx
@@ -9,13 +9,16 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
       {username !== '' && (
