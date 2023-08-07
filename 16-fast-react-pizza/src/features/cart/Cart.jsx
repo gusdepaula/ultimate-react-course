756cbe99e59b2b757adfa177e1ed0961c3307574
Gustavo de Paula <gdepaula@gmail.com>
2023-08-07 10:50:47 -0300

    Styling the Cart

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -30,16 +31,22 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -6,7 +6,9 @@ function Button({ children, disabled, to, type }) {
 
   const styles = {
     primary: base + 'px-4 py-3 md:px-6 md:py-4 ',
-    small: base + 'py-2 px-4 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-200 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4 px-4 py-2.5 md:px-6 md:py-3.5',
+    small: base + 'py-2 px-4 md:px-5 md:py-2.5 text-xs ',
   };
   if (to)
     return (
