9aafc6b798a665cafc2901dc3f7317b44029450a
Gustavo de Paula <gdepaula@gmail.com>
2023-08-08 13:49:33 -0300

    Modeling the "Cart" State

---
+++ 16-fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  // cart: [],
+  cart: {
+    pizzaId: 12,
+    name: 'Mussarela',
+    quantity: 2,
+    unitPrice: 16,
+    totalPrice: 32,
+  },
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
--- 16-fast-react-pizza/src/store.js
+++ 16-fast-react-pizza/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import useReducer from './features/user/userSlice';
+import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 
 const store = configureStore({
   reducer: {
-    user: useReducer,
+    user: userReducer,
+    cart: cartReducer,
   },
 });
 
