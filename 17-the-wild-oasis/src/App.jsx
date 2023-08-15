fba2cd3c8a345f83c447d3c27c273422ae80df29
Gustavo de Paula <gdepaula@gmail.com>
2023-08-15 11:19:26 -0300

    Introduction to Styled Components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,5 +1,42 @@
+import { styled } from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  margin: 20px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button onClick={() => console.log("checkin")}>Check-in</Button>
+      <Button onClick={() => console.log("checkout")}>Check-out</Button>
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
+  );
 }
 
 export default App;
