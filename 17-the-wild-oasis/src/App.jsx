0345c56c0f7034afe327bd5b8d32963829ad32ab
Gustavo de Paula <gdepaula@gmail.com>
2023-08-17 12:10:14 -0300

    Building More Reusable styled components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,14 +15,35 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Heading as="h2">Checkin and out</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Checkin and out</Heading>
 
-        <Button onClick={() => console.log("checkin")}>Check-in</Button>
-        <Button onClick={() => console.log("checkout")}>Check-out</Button>
-        <Heading as="h3">Form</Heading>
-
-        <Input type="number" placeholder="Number of guests" />
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => console.log("checkin")}
+              >
+                Check-in
+              </Button>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => console.log("checkout")}
+              >
+                Check-out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
--- 17-the-wild-oasis/src/ui/Button.jsx
+++ 17-the-wild-oasis/src/ui/Button.jsx
@@ -49,20 +49,17 @@ const variations = {
 };
 
 const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
   border: none;
-  border-radius: 7px;
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
+  border-radius: var(--border-radius-sm);
   box-shadow: var(--shadow-sm);
-  margin: 20px;
-  cursor: pointer;
 
-  &:hover {
-    background-color: var(--color-brand-700);
-  }
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
 `;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 
 export default Button;
