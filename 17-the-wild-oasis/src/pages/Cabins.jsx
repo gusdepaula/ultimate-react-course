89c08e1280d6bd31ef2d56b0c3edbe8e857be0b6
Gustavo de Paula <gdepaula@gmail.com>
2023-08-21 12:09:59 -0300

    Setting Up Storage Buckets

--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -11,6 +11,10 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://kifhcpebtijtkbcbwdra.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt=""
+      />
     </Row>
   );
 }
