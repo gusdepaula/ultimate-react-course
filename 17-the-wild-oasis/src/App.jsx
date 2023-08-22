ca9f2ccbc24c042d0e6e4a5a311f3c42b73b557c
Gustavo de Paula <gdepaula@gmail.com>
2023-08-22 11:17:26 -0300

    Fetching Cabin Data

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -16,7 +16,8 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
