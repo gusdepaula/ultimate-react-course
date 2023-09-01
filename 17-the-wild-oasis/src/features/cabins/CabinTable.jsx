c512ba5c8fdab6143795b7f5d09ca9cb29ea176b
Gustavo de Paula <gdepaula@gmail.com>
2023-09-01 10:26:27 -0300

    Client-Side Filtering: Filtering Cabins

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,30 +1,27 @@
-import styled from "styled-components";
-
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+
+  if (filterValue === "all") filteredCabins = cabins;
+
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
   return (
     <Menus>
@@ -39,7 +36,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
---
+++ 17-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,19 @@
+import Filter from "../../ui/Filter";
+import TableOperations from "../../ui/TableOperations";
+
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
+    </TableOperations>
+  );
+}
+
+export default CabinTableOperations;
