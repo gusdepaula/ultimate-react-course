9321a62bfa3b8a4017314a8da0a32e21217258c3
Gustavo de Paula <gdepaula@gmail.com>
2023-09-04 09:36:32 -0300

    Client-Side Sorting: Sorting Cabins

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -23,6 +23,13 @@ function CabinTable() {
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -36,7 +43,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- 17-the-wild-oasis/src/ui/Filter.jsx
+++ 17-the-wild-oasis/src/ui/Filter.jsx
@@ -51,6 +51,7 @@ function Filter({ filterField, options }) {
           key={option.value}
           onClick={() => handleClick(option.value)}
           active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
         >
           {option.label}
         </FilterButton>
