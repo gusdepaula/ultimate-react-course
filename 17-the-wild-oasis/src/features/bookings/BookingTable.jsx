ee83aa32a9f5d8957698723f98fb966efeca968e
Gustavo de Paula <gdepaula@gmail.com>
2023-09-05 11:36:17 -0300

    Building the Booking Table

--- 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -1,9 +1,15 @@
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import Empty from "../../ui/Empty";
+import { useBookings } from "./useBookings";
+import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings, isLoading } = useBookings();
+
+  if (isLoading) return <Spinner />;
+  if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
     <Menus>
---
+++ 17-the-wild-oasis/src/features/bookings/useBookings.js
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+
+export function useBookings() {
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({ queryKey: ["bookings"], queryFn: getBookings });
+
+  return { isLoading, bookings, error };
+}
