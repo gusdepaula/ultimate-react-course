4999a0b6afb31c431a58e7c93cbd95c878bf151b
Gustavo de Paula <gdepaula@gmail.com>
2023-09-06 11:05:15 -0300

    Building a Reusable Pagination Component

--- 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const { bookings, isLoading } = useBookings();
@@ -29,6 +30,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={5} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
