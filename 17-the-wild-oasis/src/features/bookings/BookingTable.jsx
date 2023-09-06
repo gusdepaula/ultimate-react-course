a50467b25b54d815356308f4eb14ed80f61c4636
Gustavo de Paula <gdepaula@gmail.com>
2023-09-06 11:18:00 -0300

    API-Side Pagination: Pagination Bookings

--- 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingTable.jsx
@@ -7,7 +7,7 @@ import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
   if (!bookings.length) return <Empty resourceName="bookings" />;
@@ -31,7 +31,7 @@ function BookingTable() {
           )}
         />
         <Table.Footer>
-          <Pagination count={5} />
+          <Pagination count={count} />
         </Table.Footer>
       </Table>
     </Menus>
--- 17-the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/src/features/bookings/useBookings.js
@@ -18,14 +18,17 @@ export function useBookings() {
   const [field, directiton] = sortByRaw.split("-");
   const sortBy = { field, directiton };
 
+  //Pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, bookings, error };
+  return { isLoading, bookings, error, count };
 }
