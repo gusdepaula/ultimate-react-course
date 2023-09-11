41352a2f4d68fbbc08e0859f94927ae423f49e7e
Gustavo de Paula <gdepaula@gmail.com>
2023-09-11 14:49:35 -0300

    Authorization: Protecting Routes

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -15,6 +15,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import Checking from "./pages/Checking";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,7 +33,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
--- 17-the-wild-oasis/src/services/apiAuth.js
+++ 17-the-wild-oasis/src/services/apiAuth.js
@@ -11,3 +11,15 @@ export async function login({ email, password }) {
   console.log(data);
   return data;
 }
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  console.log(data);
+  return data?.user;
+}
