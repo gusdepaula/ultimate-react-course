9620367ebaad377aebe8fcf476370de41169593b
Gustavo de Paula <gdepaula@gmail.com>
2023-09-11 11:49:36 -0300

    Authentication: User Login With Supabase

---
+++ 17-the-wild-oasis/src/services/apiAuth.js
@@ -0,0 +1,13 @@
+import supabase from "./supabase";
+
+export async function login({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw new Error(error.message);
+
+  console.log(data);
+  return data;
+}
