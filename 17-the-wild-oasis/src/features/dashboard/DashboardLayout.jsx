b64e49d31165871c10788cab13073e519eff8686
Gustavo de Paula <gdepaula@gmail.com>
2023-09-15 14:59:47 -0300

    Deploying to Netlify

--- 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
+import { useRecentStays } from "./useRecentStays";
 import { useRecentBookings } from "./useRecentBookings";
 import Spinner from "../../ui/Spinner";
-import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
@@ -17,12 +17,7 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { bookings, isLoading: isLoading1 } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    isLoading: isLoading2,
-    numDays,
-  } = useRecentStays();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
   const { cabins, isLoading: isLoading3 } = useCabins();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
