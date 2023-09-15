edea2c4dd2678939c4ad127f69e3df991b721c5c
Gustavo de Paula <gdepaula@gmail.com>
2023-09-15 13:58:40 -0300

    Displaying Stays for Current Day

--- 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,7 +35,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
